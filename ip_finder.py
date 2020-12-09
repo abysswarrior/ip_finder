0c5457a7fd29f686a2a188a472fca94d66cfce6d
Mehran Safaripour <52608415+Mehran-SP@users.noreply.github.com>
2020-12-09 18:37:19 +0330

    Update ip_finder.py

--- ip_finder.py
+++ ip_finder.py
@@ -1,5 +1,6 @@
 from ipify import get_ip                                                                                                                                        
                                                                                                                                                                 
 ip = get_ip()                                                                                                                                                   
-                                                                                                                                                                
+
+# mior change
 print("ip : ", ip)
