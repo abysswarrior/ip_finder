28edb51f318309063941f30621d9aadd487baa36
Mehran Safaripour <52608415+Mehran-SP@users.noreply.github.com>
2020-12-09 18:32:00 +0330

    Create ip_finder.py

---
+++ ip_finder.py
@@ -0,0 +1,5 @@
+from ipify import get_ip                                                                                                                                        
+                                                                                                                                                                
+ip = get_ip()                                                                                                                                                   
+                                                                                                                                                                
+print("ip : ", ip)
