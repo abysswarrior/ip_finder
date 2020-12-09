f29392f16e023d98aa7edcbd7efde4df6b4d876d
Mehran Safaripour <52608415+Mehran-SP@users.noreply.github.com>
2020-12-09 22:34:31 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -13,5 +13,5 @@ def home_view():
     lookup = IPWhois(ip.json()['ip']).lookup_whois()
 
     return f"<h1>Welcome to our crypto trading profile!! </br> this is running at IP/region : {ip.json()['ip']} {lookup['nets'][0]['description']} {lookup['nets'][0]['country']}</h1>"
-  except Exception as e:
-    return f"<h1>can not get ip {str(e)}</h1>"
+  except:
+    return f"<h1>can not get ip</h1>"
