85f915512ffe22ca3ef142fa4a7cef8d7c81075c
Mehran Safaripour <52608415+Mehran-SP@users.noreply.github.com>
2020-12-09 20:07:18 +0330

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from ipify import get_ip                                                                                                                                        
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():                                                                                                                                                     
-    ip = get_ip()                                                                                                                                                   
-    return str(ip)
