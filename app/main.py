f99098845b3cfc0231e5d78eaef82b03ed3b99b5
Mehran Safaripour <52608415+Mehran-SP@users.noreply.github.com>
2020-12-09 20:10:59 +0330

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 
 from flask import Flask 
+from ipify import get_ip
   
 app = Flask(__name__) 
   
 @app.route("/") 
-def home_view(): 
-        return "<h1>Welcome to Geeks for Geeks</h1>"
+def home_view():
+        ip = get_ip()
+        return f"<h1>Welcome to our crypto trading profile!! </br> this will be run at {str(ip)}</h1>"
